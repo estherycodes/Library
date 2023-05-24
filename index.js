95a0ac08cc5c40e7041697ab7488160091530447
Esther <11yekinie@gmail.com>
2023-05-24 18:44:19 +0100

    bookcard, header style

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-let myLibrary = ['The Hobbit', 'Harry Potter', 'Bridgerton'];
+/* eslint-disable space-infix-ops */
+/* eslint-disable prefer-const */
+let myLibrary = [];
 
-function book(title, author) {
+function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
 function addBookToLibrary(title, author) {
-    let book = New book(title, author);
-    myLibrary.push(book);
+  let novel = new Book(title, author);
+  myLibrary.push(novel);
 }
 
 // Adding some books to the library
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald");
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee");
-addBookToLibrary("1984", "George Orwell");
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald');
+addBookToLibrary('To Kill a Mockingbird', 'Harper Lee');
+addBookToLibrary('1984', 'George Orwell');
+
+// book colors
+let colors = ['#EF7E6B', '#98D4E1', '#D7C8B5', '#FDD28A'];
 
 function displayBooks() {
-
   const main = document.querySelector('main');
 
   myLibrary.forEach((book, index) => {
     const div = document.createElement('div');
+    div.classList.add('bookCard');
 
-    let bookText = `Book ${index+1}:<br>`;
-    bookText += `Title: ${book.title}<br>`;
-    bookText += `Author: ${book.author}<br>`;
-    bookText += '------------------------';
+    let bookText = `${book.title}<br>`;
+    bookText += `By: ${book.author}<br>`;
 
     div.innerHTML = bookText;
-    
+
+    div.style.backgroundColor = colors[index % colors.length];
+
     main.appendChild(div);
   });
 }
 
-displayBooks(); 
+displayBooks();
